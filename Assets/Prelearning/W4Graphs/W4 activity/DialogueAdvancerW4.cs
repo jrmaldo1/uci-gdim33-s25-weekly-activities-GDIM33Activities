063a30a6a1f0b605d5f2d129abaf6c473a0498ec
jrmaldo1 <jrmaldo1@uci.edu>
2026-04-22 19:14:15 -0700

    :3

--- Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
+++ Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
@@ -8,10 +8,7 @@ namespace W4Activity
 {
     public class DialogueAdvancerW4 : MonoBehaviour
     {
-        private void Start()
-        {
-            NewDialogueEventW4
-        }
+       
         // Singleton stuff- allows this class to be accessed from anywhere in the codebase without a reference (like a member variable)
         // do not edit this line
         public static DialogueAdvancerW4 _Instance {get; private set;}
@@ -34,6 +31,11 @@ namespace W4Activity
         public void ChooseDialogue (DialogueNodeW4 nextLine)
         {
             EventBus.Trigger(VsEventNames.NewDialogueEvent, nextLine);
+        }
+
+        private void Start()
+        {
+            ChooseDialogue(dialogueNode);
         }
     }
 }
